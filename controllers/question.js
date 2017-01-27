c121c0a7bf0071444691375aefe63523537238ee
thisisdax <weihua@live.com.sg>
2017-01-27 08:58:49 +0800

    edit a bug when host deletes room and user doesn't redirect back to list page

--- controllers/question.js
+++ controllers/question.js
@@ -9,6 +9,7 @@ router.get('/list/:id', function (req, res) {
   if (req.user) {
     Room.findById(req.params.id).populate('question').exec(function (err, room) {
       if (err) return console.log(err)
+      return res.redirect('/list')
       Poll.find({room: req.params.id, running: true}, function (err, check) {
         if (err) return console.log(err)
         res.render('room/lobby', {room: room, user: req.user, check: check, key: req.params.id})
